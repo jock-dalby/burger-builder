9aa6f27803b2f82759ec79bde960c3083ac50d00
jock-dalby <jockdalby86@gmail.com>
2018-04-03 06:58:34 +1200

    add BurgerBuilder component, add Content class to main element, fix props.children typo bug

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import classes from './Layout.css';
 
 const Layout = (props) => (
   <Aux>
     <div>
       Toolbar, Sidedrawer, Backdrop
     </div>
-    <main>
-      {props.chidren}
+    <main className={classes.Content}>
+      {props.children}
     </main>
   </Aux>
 )
