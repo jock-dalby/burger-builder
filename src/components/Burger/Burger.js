501484c8ddd76393df17ce58c6b739adebf3c295
jock-dalby <jockdalby86@gmail.com>
2018-04-05 17:42:40 +1200

    pass and dynamically create burger ingredients from state in the BurgerBuilder container into Burger component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,17 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(key => {
+      return [...Array(props.ingredients[key])] // create an empty array to map over, returns [ , ]
+      // use the index to create a unique key for each ingredient.
+        .map((_, i) => <BurgerIngredient key={key + i} type={key}/>)
+    })
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top"/>
-      <BurgerIngredient type="cheese"/>
-      <BurgerIngredient type="meat"/>
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom"/>
     </div>
   );
