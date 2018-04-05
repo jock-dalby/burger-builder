b683396ba3a67da07c5f4af6dee87e8656408313
jock-dalby <jockdalby86@gmail.com>
2018-04-05 17:48:56 +1200

    add reduce funtion to transformedIngredients method to enable message to appear when no ingredients added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -9,11 +9,14 @@ const burger = (props) => {
       return [...Array(props.ingredients[key])] // create an empty array to map over, returns [ , ]
       // use the index to create a unique key for each ingredient.
         .map((_, i) => <BurgerIngredient key={key + i} type={key}/>)
-    })
+    }).reduce((arr, elem) => {
+      return arr.concat(elem);
+    }, []);
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top"/>
-      {transformedIngredients}
+      {transformedIngredients.length > 0 ? transformedIngredients : 'Start adding ingredients'}
       <BurgerIngredient type="bread-bottom"/>
     </div>
   );
