453bb73d9c88d79e4a55947bd17ee472e4d7354d
jock-dalby <jockdalby86@gmail.com>
2018-04-11 18:31:44 +1200

    add Modal and Backdrop files and import and display in BurgerBuilder.js

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -73,6 +74,7 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
+        <Modal />
         <Burger ingredients={this.state.ingredients}/>
         <BuildControls ingredients={this.state.ingredients}
           addIngredient={this.addIngredientHandler}
