e69a258b95e16d2de63af8f65857b4212802a86b
jock-dalby <jockdalby86@gmail.com>
2018-04-17 18:44:13 +1200

    move Layout.js to hoc folder

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import { jsUcfirst } from '../../../common/HelperFunctions';
 import Button from '../../UI/Button/Button';
 
