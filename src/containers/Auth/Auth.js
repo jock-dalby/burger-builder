d484c0e4866be4cb999ab3970d0918ac0c2e0ea2
jock-dalby <jockdalby86@gmail.com>
2018-06-23 18:10:32 +1200

    add auth form, css and validation

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 
+import classes from './Auth.css';
+
 class Auth extends Component {
   state = {
     controls: {
@@ -37,25 +39,66 @@ class Auth extends Component {
     }
   }
 
-  onChangeHandler = (a, b) => {
+  checkValidity = (value, rules) => {
+    let isValid = true;
+    if (!rules) {
+      return true;
+    }
 
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid
+    }
+
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid
+    }
+
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid
+    }
+
+    return isValid;
   }
+
+  onChangeHandler = (event, controlName) => {
+    const controls = {
+      ...this.state.controls,
+      [controlName]: {
+        ...this.state.controls[controlName],
+        value: event.target.value,
+        valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+        touched: true
+      }
+    };
+    this.setState({ controls });
+  }
+
   render() {
     const formElements = Object.keys(this.state.controls).map(key => (
       <Input label={key}
         key={key}
         valueType={key}
-        invalid={!this.state.controls[key].valid}
-        touched={this.state.controls[key].touched}
-        shouldValidate={this.state.controls[key].validation}
+        invalid={!this.state.controls[ key ].valid}
+        touched={this.state.controls[ key ].touched}
+        shouldValidate={this.state.controls[ key ].validation}
         onChangeHandler={(event) => this.onChangeHandler(event, key)}
-        inputtype={this.state.controls[key].elementType}
-        elementConfig={this.state.controls[key].elementConfig}
-        value={this.state.controls[key].value}
-        />
+        inputtype={this.state.controls[ key ].elementType}
+        elementConfig={this.state.controls[ key ].elementConfig}
+        value={this.state.controls[ key ].value}
+      />
     ));
     return (
-      <div>
+      <div className={classes.Auth}>
         <form>
           {formElements}
           <Button btnType="Success">SUBMIT</Button>
