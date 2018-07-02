75f689f8db395777e768afcb8aa06624e314707e
jock-dalby <jockdalby86@gmail.com>
2018-07-02 18:26:42 +1200

    Only activate Redux devtools when in Dev mode

--- src/index.js
+++ src/index.js
@@ -12,7 +12,11 @@ import authReducer from './store/reducers/auth';
 
 import { BrowserRouter } from 'react-router-dom';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// Only activate Redux devtools when in Dev mode. NODE-ENV is defined in config/env.js
+const composeEnhancers = process.env.NODE_ENV === 'development'
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  : null
+  || compose;
 
 // Slices of state
 const rootReducer = combineReducers({
