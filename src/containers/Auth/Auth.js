56628b41457a41721e9deb72ca80acef801a4646
jock-dalby <jockdalby86@gmail.com>
2018-07-02 18:23:18 +1200

    move validation to shared resource

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -6,7 +6,7 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import * as actions from '../../store/actions/index';
-import { updateObject } from '../../shared/utility';
+import { updateObject, checkValidity } from '../../shared/utility';
 
 import classes from './Auth.css';
 
@@ -54,37 +54,6 @@ class Auth extends Component {
     }
   }
 
-  checkValidity = (value, rules) => {
-    let isValid = true;
-    if (!rules) {
-      return true;
-    }
-
-    if (rules.required) {
-      isValid = value.trim() !== '' && isValid;
-    }
-
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid
-    }
-
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid
-    }
-
-    if (rules.isEmail) {
-      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-      isValid = pattern.test(value) && isValid
-    }
-
-    if (rules.isNumeric) {
-      const pattern = /^\d+$/;
-      isValid = pattern.test(value) && isValid
-    }
-
-    return isValid;
-  }
-
   onChangeHandler = (event, controlName) => {
     const controls = updateObject(
       this.state.controls,
@@ -93,7 +62,7 @@ class Auth extends Component {
           this.state.controls[controlName],
           {
             value: event.target.value,
-            valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+            valid: checkValidity(event.target.value, this.state.controls[controlName].validation),
             touched: true
           }
         )
--- src/containers/Checkout/ContactDetails/ContactDetails.js
+++ src/containers/Checkout/ContactDetails/ContactDetails.js
@@ -7,7 +7,7 @@ import axios from '../../../axios-orders';
 import { connect } from 'react-redux';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
-import { updateObject } from '../../../shared/utility';
+import { updateObject, checkValidity } from '../../../shared/utility';
 
 class ContactDetails extends Component {
   state = {
@@ -115,37 +115,6 @@ class ContactDetails extends Component {
     this.props.onPurchaseBurger(orderData, this.props.token);
   }
 
-  checkValidity = ( value, rules ) => {
-    let isValid = true;
-    if ( !rules ) {
-        return true;
-    }
-
-    if ( rules.required ) {
-        isValid = value.trim() !== '' && isValid;
-    }
-
-    if ( rules.minLength ) {
-        isValid = value.length >= rules.minLength && isValid
-    }
-
-    if ( rules.maxLength ) {
-        isValid = value.length <= rules.maxLength && isValid
-    }
-
-    if ( rules.isEmail ) {
-        const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-        isValid = pattern.test( value ) && isValid
-    }
-
-    if ( rules.isNumeric ) {
-        const pattern = /^\d+$/;
-        isValid = pattern.test( value ) && isValid
-    }
-
-    return isValid;
-}
-
   onBlurHandler = (id) => {
     const orderForm = {
       ...this.state.orderForm
@@ -161,7 +130,7 @@ class ContactDetails extends Component {
       this.state.orderForm[id],
       {
         value: event.target.value,
-        valid: this.checkValidity(event.target.value, this.state.orderForm[id].validation),
+        valid: checkValidity(event.target.value, this.state.orderForm[id].validation),
         touched: true
       }
     )
