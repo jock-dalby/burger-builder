915b40a4b9078ee41d9acd87715d9d8644540f83
jock-dalby <jockdalby86@gmail.com>
2018-07-04 18:04:08 +1200

    write first unit test for NavigationItems

---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// enzyme is what allows us to render the <NavigationItems /> component as a standalone component
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import NavigationItems from './NavigationItems';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+// enzyme is now connected
+configure({adapter: new Adapter()});
+
+describe('<NavigationItems />', () => {
+  it('should render two <NavigationItem /> elements if not authenticated', () => {
+    // create an instance of the rendered component.
+    const wrapper = shallow(<NavigationItems />); //jsx
+    // using shallow only renders child component's as placeholders and does not actually render the code from
+    // the child components. Essentially rendering the code exactly as we see it in the component template.
+    expect(wrapper.find(NavigationItem)).toHaveLength(2); // not jsx
+  });
+});
