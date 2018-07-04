cd87e12ace0feed49d9b53c1d7b22230b1c03cae
jock-dalby <jockdalby86@gmail.com>
2018-07-04 18:28:25 +1200

    write test to check for specific template elements

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -10,16 +10,42 @@ import NavigationItem from './NavigationItem/NavigationItem';
 configure({adapter: new Adapter()});
 
 describe('<NavigationItems />', () => {
-  it('should render two <NavigationItem /> elements if not authenticated', () => {
+
+  let wrapper;
+  const loginNavigationItem = <NavigationItem link='/auth'>Log in</NavigationItem>;
+  const logoutNavigationItem = <NavigationItem link='/logout'>Log out</NavigationItem>;
+
+  beforeEach(() => {
     // create an instance of the rendered component.
-    const wrapper = shallow(<NavigationItems />); //jsx
     // using shallow only renders child component's as placeholders and does not actually render the code from
     // the child components. Essentially rendering the code exactly as we see it in the component template.
+    wrapper = shallow(<NavigationItems />); //jsx
+    // TO FIND OUT HOW TO RENDER THE FULL COMPONENT TREE SEE ENZYME DOCUMENTATION
+  })
+
+  it('should render two <NavigationItem /> elements if is NOT authenticated', () => {
+    // FOR MORE UTITLITY FUNCTIONS, HOW TO MOCK SERVIES ETC. SEE JEST DOCUMENTATION
     expect(wrapper.find(NavigationItem)).toHaveLength(2); // not jsx
   });
 
-  it('should render three <NavigationItem /> elements if is authenticated', () => {
-    const wrapper = shallow(<NavigationItems isAuthenticated='true'/>);
-    expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  it(`should render 'Log out' <NavigationItem /> element if is NOT authenticated`, () => {
+    expect(wrapper.contains(loginNavigationItem)).toEqual(true);
+    expect(wrapper.contains(logoutNavigationItem)).toEqual(false);
   });
+
+  describe('if user is authenticated', () => {
+    beforeEach(() => {
+      wrapper.setProps({isAuthenticated: true});
+    });
+
+    it('should render three <NavigationItem /> elements if is authenticated', () => {
+      expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+
+    it(`should render 'Log out' <NavigationItem /> element if is authenticated`, () => {
+      expect(wrapper.contains(loginNavigationItem)).toEqual(false);
+      expect(wrapper.contains(logoutNavigationItem)).toEqual(true);
+    });
+  })
+
 });
